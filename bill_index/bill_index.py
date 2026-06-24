ababc1b4c610166bb658afbe95d020d376e972f4
Rolf Hendriks <rolf@rolfhendriks.net>
2026-06-24 09:26:51 -0400

    Fixed linting errors for refactored comments

--- bill_index/bill_index.py
+++ bill_index/bill_index.py
@@ -1,13 +1,15 @@
 """
-A cache for bill metadata, intended to accumulate information about large volumes of bills.
+A cache for bill metadata intended to accumulate information about large volumes of bills.
 
 The guiding idea behind the cache is that each bill has an identifying slug of the form:
 
-{congress}-{type}-{number}
+{congress}-{type}-{number}[:{version}]
 
 e.g. `119-hr-1` for the 1st House Resolution bill of the 119th Congress.
 
-Aside from a uniquely identifying slug, each bill can have arbitrary metadata. Adding a bill to the index will automatically update metadata if a bill with the same ID exists already, otherwise create a new record. The bill index can be used to prevent duplicate downloads when managing large volumes of bill data or to combine metadata from different sources.
+Aside from a uniquely identifying slug, each bill can have arbitrary metadata.
+The index automatically syncs with a CSV file. It can be used to prevent duplicate downloads
+and to accumulate bill metadata from different sources.
 
 Usage:
 index = BillIndex(csv_path)
