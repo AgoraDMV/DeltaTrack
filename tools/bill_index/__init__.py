3aa84963bf5de285759258665026278f7b011aef
Rolf Hendriks <rolf@rolfhendriks.net>
2026-08-12 22:15:09 -0400

    moved bill index into shared folder

--- tools/bill_index/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Bill index helpers."""
-
-from .bill_index import BillIdentifier, BillIndex, InsertMode, make_bill_id, parse_bill_id
-
-__all__ = ["BillIndex", "InsertMode", "make_bill_id", "BillIdentifier", "parse_bill_id"]
--- tools/fetch_bill_archives.py
+++ tools/fetch_bill_archives.py
@@ -19,7 +19,7 @@ from typing import Any
 
 import httpx
 
-from bill_index import BillIndex, make_bill_id
+from shared.bill_index import BillIndex, make_bill_id
 from shared.bill_types import BILL_TYPES, resolve_bill_types
 from shared.http import download_archives as http_download_archives
 from shared.zip import extract_archive
